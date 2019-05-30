e480ed2e968f9eb3e27313ec27e159eea11036d8
Manuel Alférez Ruiz <manuel@MacBook-Air-de-Manuel.local>
2019-05-30 11:06:59 +0200

    feat: Card content, new design buttons

--- src/Component/Home/Card.js
+++ src/Component/Home/Card.js
@@ -1,12 +1,69 @@
 import React from "react";
-import './Card.css';
+import "./Card.css";
+import profilePicture from "../../images/Home/profile.png";
+import socialData from "../../data/SocialData";
+/* import cardData from '../../Data/CardData'; */
 
 function Card(props) {
   return (
-      <div className='Card'>
-          
+    <div className="Card">
+      <div className="Card__header">
+        <img className="Header__picture" src={profilePicture} />
+        <div className="Header__description">
+          <p>Hi! I'm Manuel Alférez Ruiz</p>
+          <p>I live in Jaén, Spain</p>
+          <br />
+          <p>Developing | Sharing | Kaizen</p>
+        </div>
       </div>
-  )
+      <div className="Card__bio">
+        <h1 className="Bio__title">Little about me</h1>
+        <div className="Bio__description">
+          <p>
+            At the moment, I am learning Front-end and English. At the same time
+            we help grow the
+            <a
+              href="https://twitter.com/GdgJaen"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              GDG Jaén
+            </a>
+            community and I study computer engineering at
+            <a
+              href="https://www.ujaen.es/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              UJA
+            </a>
+          </p>
+          <p>The future ... the future is exciting.</p>
+        </div>
+      </div>
+      <div className="Card__social">
+        {socialData.map(item => (
+          <a
+            className={item.className}
+            href={item.dir}
+            key={item.key}
+            target="_blank"
+            rel="noopener noreferrer"
+          />
+        ))}
+      </div>
+      <div className="Card__footer">
+        Designed and made with ❤️ by
+        <a
+          href="https://twitter.com/manuelalferez "
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Manuel Alférez
+        </a>
+      </div>
+    </div>
+  );
 }
 
 export default Card;
---
+++ src/data/SocialData.js
@@ -0,0 +1,24 @@
+const data = [
+    {
+      key: 1,
+      dir: "https://twitter.com/manuelalferez",
+      className: "icon-twitter"
+    },
+    {
+      key: 2,
+      dir: "https://github.com/manuelalferez",
+      className: "icon-github"
+    },
+    {
+      key: 3,
+      dir: "https://www.instagram.com/manuelalferez_",
+      className: "icon-instagram"
+    },
+    {
+      key: 4,
+      dir: "https://www.linkedin.com/in/manuelalferez",
+      className: "icon-linkedin"
+    }
+  ];
+  
+  export default data;
