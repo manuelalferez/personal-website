7ec32a264102a90456607d3424e96199dd6e0e39
Manuel <manuelalferezruiz@gmail.com>
2019-08-06 18:59:12 +0200

    feat: google analytics added

--- src/App.js
+++ src/App.js
@@ -6,10 +6,18 @@ import PageNotFound from "./Component/PageNotFound/PageNotFound";
 import Contact from "./Component/Contact/Contact";
 import Layout from "./Component/Layout/Layout";
 import Post from "./Component/Blog/Post";
+import ReactGA from "react-ga";
+
+ReactGA.initialize("UA-135366462-1");
+
+const onUpdate = () => {
+  ReactGA.set({ page: window.location.pathname });
+  ReactGA.pageview(window.location.pathname);
+};
 
 function App() {
   return (
-    <HashRouter>
+    <HashRouter onUpdate={onUpdate}>
       <Layout>
         <Switch>
           <Route exact path="/" component={Home} />
