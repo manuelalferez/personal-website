445b459c593866ab6027a4323b1f9a721df51474
Manuel <manuelalferezruiz@gmail.com>
2019-08-09 14:35:53 +0200

    fix: Google Analytics. Now each page is traking

--- src/App.js
+++ src/App.js
@@ -6,20 +6,18 @@ import PageNotFound from "./Component/PageNotFound/PageNotFound";
 import Contact from "./Component/Contact/Contact";
 import Layout from "./Component/Layout/Layout";
 import Post from "./Component/Blog/Post";
-import ReactGA from "react-ga";
-
-ReactGA.initialize("UA-129135896-1");
-ReactGA.pageview(window.location.pathname + window.location.search);
+import GA from "./GoogleAnalytics";
 
 function App() {
   return (
     <HashRouter>
+      {GA.init() && <GA.RouteTracker />}
       <Layout>
         <Switch>
-          <Route exact path="/" component={Home}/>
-          <Route exact path="/contact" component={Contact}/>
-          <Route exact path="/blog" component={Blog}/>
-          <Route exact path="/blog/post/:id" component={Post}/>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/contact" component={Contact} />
+          <Route exact path="/blog" component={Blog} />
+          <Route exact path="/blog/post/:id" component={Post} />
           <Route component={PageNotFound} />
         </Switch>
       </Layout>
