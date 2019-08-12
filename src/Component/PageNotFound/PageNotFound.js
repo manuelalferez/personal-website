b489612de76c061f3f0e106c73da765be1e72e05
Manuel <manuelalferezruiz@gmail.com>
2019-08-12 22:33:23 +0200

    feat: page implementation not found

--- src/Component/PageNotFound/PageNotFound.js
+++ src/Component/PageNotFound/PageNotFound.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import React from "react";
+import "./PageNotFound.css";
+import { Link } from "react-router-dom";
 
-function PageNotFound(props){
-    return <div>PageNotFound</div>;
+import lost_IMG from "../../images/PageNotFound/lost.png";
+
+function PageNotFound(props) {
+  return (
+    <div className="PageNotFound">
+      <img
+        src={lost_IMG}
+        alt="Page not found"
+        className="PageNotFound__image"
+      />
+      <h1 className="PageNotFound__text">The requested page does not exist</h1>
+    </div>
+  );
 }
 
 export default PageNotFound;
