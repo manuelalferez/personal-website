963279c2ca5a47c71b700043b3a73e332ff3891d
Manuel Alférez Ruiz <manuelalferezruiz@gmail.com>
2020-01-16 13:22:13 +0100

    feat: nuevo articulo

--- src/data/Blog/PostsData.js
+++ src/data/Blog/PostsData.js
@@ -222,6 +222,69 @@ const posts = [
     familiares, amigos, conocidos y personas con las que rara vez hablamos) al mantener nuestro 
     crecimiento latente y compartiéndolo en nuestra red, ayudamos a construir una red más fuerte. </p>
      `
+  },
+  {
+    title: "La productividad en el aprendizaje",
+    category: "Reflexiones",
+    summary: ["Cómo la maquetación del conocimiento de manera estética afecta a la forma que aprendemos."],
+    key: "8",
+    date: "Enero 16, 2020",
+    content: `<p class='Citation'>Es imposible e incluso contraproducente que alguien trate de innovar una tarea 
+    mientras le pedimos que sea eficiente realizándola.</br>
+    <b><a href="https://www.goodreads.com/book/show/12524490-innovar-para-ganar" target="_blank" 
+    rel="noopener noreferrer">Innovar para ganar" (2011)</a></b>
+    </p>
+    
+    <p>La perfección aplicada a cada tarea que realizamos tiene un coste. Esto lo podemos ver 
+    cuando el número de tareas que realizamos decrece a causa del excesivo tiempo que invertimos 
+    en optimizar el sistema.</p>
+    
+    <p>Cuando se trata de aprendizaje la cosa se pone peor. En nuestro proceso de aprendizaje 
+    inherentemente cometemos errores. Esos errores iniciales son parte del conocimiento y el 
+    hecho de estar enfocados en el rendimiento detiene en seco nuestro clímax de aprendizaje.</p>
+    
+    <h2>¿La productividad aplicada al aprendizaje nos hace menos creativos?</h2>
+    
+    <p>Ya lo vimos con <a href="https://www.youtube.com/channel/UCDrekHmOnkptxq3gUU0IyfA" 
+    target="_blank" rel="noopener noreferrer">Devon 
+    Crawford</a>. Devon nos demuestra como el proceso de aprendizaje 
+    nos sumerge en un mar de incertidumbre y con sus vídeos, desde una visión en primera 
+    persona, nos enseña cómo es la realidad si elegimos aprender algo completamente nuevo y 
+    desconocido.</p>
+    
+    <p>De él aprendimos la teoría aplicada de ensaño y error y su tenaz determinación. 
+    Podríamos extraer, por tanto, una máxima:</p>
+    
+    <p class='Citation'>En el momento de aprender con filtros reducimos el número de datos que pueden 
+    llegar a nosotros. Dentro del caos, multitud de información llegará a nosotros, alguna 
+    útil y otra no tanto y esto nos da una visión más general de lo que estamos aprendiendo.
+     Por último y no menos importante, nos aporta variedad de fuentes.</p>
+    
+    <p>Además con Devon aprendimos que estamos habituados a conseguir el conocimiento desde 
+    una fuente esquematizada y presentada de una manera visual muy placentera.</p>
+    
+    <h2>¿Qué sucede cuando el conocimiento no se nos presenta de este modo?</h2>
+    <p>La gran mayoría del conocimiento modernos están en las páginas web que los propios 
+    descubridores plasman en ellas. No todas las páginas web siguen una guía de estilo uniforme, 
+    tal como sí lo hacen las línea editoriales con los libros de las escuelas públicas. Por ello, 
+    vemos muchas diferencias de unas páginas a otras, unas plasma el conocimiento en bruto 
+    (sin ningún estilo de diseño) y otras con algo más de estilo. </p>
+    
+    <h2>¿Nos debería de importar tanto el estilo a la hora de aprender?</h2>
+    <p>Estamos aprendiendo, los detalles visuales son menos importantes y la idea se convierte 
+    en el pilar que sostiene el aprendizaje. </p>
+    
+    <p>Sugiero aprender a aprender con páginas de todos los estilos y todas las fuentes posibles. 
+    Quizás estes pensando que se puede estilizar todas las páginas que deseemos con herramientas 
+    como <a href="https://app.getpocket.com/" target="_blank" rel="noopener noreferrer">Pocket</a>, 
+    que nos guardan las páginas y nos las presentan bajo un estilo estandarizado. 
+    Y yo te pregunto, ¿de verdad compensa todo el tiempo que gastas en administrar toda esa 
+    lista de Pocket sólo para darte el placer de visualizar el contenido adecuadamente? </p>
+    
+    <p>Probemos esto: Enfoquémonos en las fuentes en bruto. Sin importar la visualización. 
+    Sumerjámonos en la red de páginas, la multitud de referencias dentro de un artículo y no 
+    tengamos ningún reparo a la estética. </p>
+     `
   }
 ];
 
