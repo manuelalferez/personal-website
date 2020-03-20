3fdf61abc26f24443846bcc22c9d5d8da720e836
Manuel Alférez Ruiz <manuelalferezruiz@gmail.com>
2020-03-20 19:46:15 +0100

    feat: Se ha añadido el enlace de Telegram al menú social

--- src/data/Home/SocialData.js
+++ src/data/Home/SocialData.js
@@ -1,21 +1,27 @@
 const data = [
-    {
+   {
       key: 1,
+      dir: "https://t.me/manuelalferez",
+      className: "icon-telegram"
+    },
+  
+    {
+      key: 2,
       dir: "https://twitter.com/manuelalferez",
       className: "icon-twitter"
     },
     {
-      key: 2,
+      key: 3,
       dir: "https://github.com/manuelalferez",
       className: "icon-github"
     },
     {
-      key: 3,
+      key: 4,
       dir: "https://www.instagram.com/manuelalferez_",
       className: "icon-instagram"
     },
     {
-      key: 4,
+      key: 5,
       dir: "https://www.linkedin.com/in/manuelalferez",
       className: "icon-linkedin"
     }
